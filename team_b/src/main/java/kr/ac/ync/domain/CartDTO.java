2c3ee905a09075ca76c09a4a2b4d2370339d3759
Administrator <Administrator@DESKTOP-N4S2IAG>
2021-12-06 09:48:04 +0900

    CartDTO

---
+++ team_b/src/main/java/kr/ac/ync/domain/CartDTO.java
@@ -0,0 +1,109 @@
+package kr.ac.ync.domain;
+
+public class CartDTO {
+	   private int cart_num;
+	    
+	    private String USERID;
+	    
+	    private int game_num;
+	    
+	    private int QUANTITY;
+	    
+	    
+	    private String title;
+	    
+	    private int price;
+	    
+	    private double sale_price;
+	    
+	    private int salePrice;
+	    
+	    private int totalPrice;
+
+	    public void initSaleTotal() {
+			this.salePrice = (int) (this.price * (1-this.sale_price));
+			this.totalPrice = this.salePrice*this.QUANTITY;
+		}
+
+		public int getCart_num() {
+			return cart_num;
+		}
+
+		public void setCart_num(int cart_num) {
+			this.cart_num = cart_num;
+		}
+
+		public String getUSERID() {
+			return USERID;
+		}
+
+		public void setUSERID(String uSERID) {
+			USERID = uSERID;
+		}
+
+		public int getGame_num() {
+			return game_num;
+		}
+
+		public void setGame_num(int game_num) {
+			this.game_num = game_num;
+		}
+
+		public int getQUANTITY() {
+			return QUANTITY;
+		}
+
+		public void setQUANTITY(int qUANTITY) {
+			QUANTITY = qUANTITY;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+
+		public int getPrice() {
+			return price;
+		}
+
+		public void setPrice(int price) {
+			this.price = price;
+		}
+
+		public double getSale_price() {
+			return sale_price;
+		}
+
+		public void setSale_price(double sale_price) {
+			this.sale_price = sale_price;
+		}
+
+		public int getSalePrice() {
+			return salePrice;
+		}
+
+		public void setSalePrice(int salePrice) {
+			this.salePrice = salePrice;
+		}
+
+		public int getTotalPrice() {
+			return totalPrice;
+		}
+
+		public void setTotalPrice(int totalPrice) {
+			this.totalPrice = totalPrice;
+		}
+
+		@Override
+		public String toString() {
+			return "CartDTO [cart_num=" + cart_num + ", USERID=" + USERID + ", game_num=" + game_num + ", QUANTITY="
+					+ QUANTITY + ", title=" + title + ", price=" + price + ", sale_price=" + sale_price + ", salePrice="
+					+ salePrice + ", totalPrice=" + totalPrice + "]";
+		}
+		
+
+	    
+}
